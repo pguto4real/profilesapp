50ae23e7c786d2b3e8f83e90429619433221a06a
Oluwamuibi Igbokwe <igbokweudoka@gmail.com>
2025-09-18 22:59:03 -0500

    new commit

---
+++ amplify.tf
@@ -0,0 +1,35 @@
+
+
+# Amplify App
+resource "aws_amplify_app" "react_app" {
+  name       = "my-react-app"
+  repository = "https://github.com/pguto4real/profilesapp.git"
+
+  # Build spec for React app
+  build_spec = <<EOT
+version: 1
+frontend:
+  phases:
+    preBuild:
+      commands:
+        - npm ci
+    build:
+      commands:
+        - npm run build
+  artifacts:
+    baseDirectory: build
+    files:
+      - '**/*'
+  cache:
+    paths:
+      - node_modules/**/*
+EOT
+}
+
+# Branch (main) with auto build enabled
+resource "aws_amplify_branch" "main" {
+  app_id            = aws_amplify_app.react_app.id
+  branch_name       = "main"       # must match your GitHub branch name
+  enable_auto_build = true         # 👈 ensures auto deploy
+  framework         = "React"      # helps Amplify detect framework
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "github_token" {
+  description = "GitHub personal access token for Amplify connection"
+  type        = string
+  sensitive   = true
+}
